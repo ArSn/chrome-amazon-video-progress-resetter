111655bc3fe2ff514cad6e870e605620bb161f87
ArSn <koljaz@web.de>
2020-01-12 23:02:18 +0100

    First version with just a button enabling on amazon.

---
+++ background.js
@@ -0,0 +1,16 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { hostContains: '.amazon.' },
+                    })
+                ],
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
