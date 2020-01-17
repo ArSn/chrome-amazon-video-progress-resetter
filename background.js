cf11ac6326aeb5a431590cd1d51f1405178bfb84
ArSn <koljaz@web.de>
2020-01-17 22:20:01 +0100

    Reformatted code. Added parsing of episodes on current page.

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { hostContains: '.amazon.' },
-                    })
-                ],
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
-    });
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: {hostContains: '.amazon.'},
+					})
+				],
+				actions: [new chrome.declarativeContent.ShowPageAction()]
+			}
+		]);
+	});
 
-    chrome.pageAction.onClicked.addListener(function (tab) {
+	chrome.pageAction.onClicked.addListener(function (tab) {
 
-        console.log('clicked...');
-        chrome.tabs.sendMessage(tab.id, {text: 'reset_progress'});
+		console.log('clicked...');
+		chrome.tabs.sendMessage(tab.id, {text: 'reset_progress'});
 
-    });
+	});
 });
 
 
--- parser.js
+++ parser.js
@@ -3,23 +3,47 @@
 console.log('start of parser');
 
 function pageLooksLikeSeasonPage() {
-    return true;
+	// todo: check whether page looks like video page and there are episodes to reset
+	return true;
+}
+
+function parseEpisodes() {
+	let titles = document.querySelectorAll('.js-episode-title-name span');
+	if (!titles) {
+		return false;
+	}
+
+	let names = [];
+
+	titles.forEach(function (element, index) {
+		names.push({
+            'index': index,
+            'title': element.innerHTML,
+        });
+	});
+
+	console.log(names);
 }
 
 
 chrome.runtime.onMessage.addListener(function (msg) {
 
-    console.log('message received');
+	console.log('message received');
 
-    if (msg.text === 'reset_progress') {
-        console.log('parsing ...');
-        if (pageLooksLikeSeasonPage()) {
-            let confirmation = confirm('Are you sure you want to reset the progress?');
-            console.log('continue resetting? ', confirmation);
-        } else {
-            alert('No seasons found on this page');
-        }
-    }
+	if (msg.text === 'reset_progress') {
+		console.log('parsing ...');
+
+		parseEpisodes();
+
+		return;
+		if (pageLooksLikeSeasonPage()) {
+			let confirmation = confirm('Are you sure you want to reset the progress?');
+			console.log('continue resetting? ', confirmation);
+			// todo: implement actual resetting here
+		} else {
+			alert('No seasons found on this page');
+		}
+	}
 });
 
 console.log('parser registered');
