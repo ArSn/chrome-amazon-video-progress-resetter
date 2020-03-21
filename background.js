a0a47fd264be70e1879cd69c350c5d2bc090405c
ArSn <koljaz@web.de>
2020-03-21 17:24:49 +0100

    Made registering event listeners in background script happening on root level instead of onInstalled listener since that is not what it is for.

--- background.js
+++ background.js
@@ -57,75 +57,74 @@ function reportResetStatus()
 	});
 }
 
-chrome.runtime.onInstalled.addListener(function () {
+chrome.pageAction.onClicked.addListener(function (tab) {
 
-	chrome.pageAction.onClicked.addListener(function (tab) {
+	kazDebug('clicked...');
+	chrome.tabs.sendMessage(tab.id, {text: 'get_reset_confirmation'});
 
-		kazDebug('clicked...');
-		chrome.tabs.sendMessage(tab.id, {text: 'get_reset_confirmation'});
+});
 
-	});
+chrome.runtime.onMessage.addListener(function (msg, sender) {
 
-	chrome.runtime.onMessage.addListener(function (msg, sender) {
+	kazDebug('background received message with text: ' + msg.text);
 
-		if (msg.text === 'trigger_reset') {
-			kazDebug('background received task to reset the following episodes');
-			backgroundSenderTabId = sender.tab.id;
-			backgroundTotalEpisodeCount = msg.items.length;
-			episodeList = msg.items;
+	if (msg.text === 'trigger_reset') {
+		kazDebug('background received task to reset the following episodes');
+		backgroundSenderTabId = sender.tab.id;
+		backgroundTotalEpisodeCount = msg.items.length;
+		episodeList = msg.items;
+		reportResetStatus();
+		resetEpisodes();
+	} else if (msg.text === 'hide_page_action') {
+		kazDebug('hiding tab with id ' + sender.tab.id);
+		chrome.pageAction.hide(sender.tab.id);
+		chrome.pageAction.setIcon({
+			tabId: sender.tab.id,
+			path: {
+				20: 'images/logo-20-gray.png',
+				40: 'images/logo-40-gray.png',
+				60: 'images/logo-60-gray.png',
+				80: 'images/logo-80-gray.png',
+				200: 'images/logo-200-gray.png',
+				668: 'images/logo-668-gray.png',
+			},
+		});
+	} else if (msg.text === 'show_page_action') {
+		kazDebug('showing tab with id ' + sender.tab.id);
+		chrome.pageAction.show(sender.tab.id);
+		chrome.tabs.executeScript(sender.tab.id, {file: 'parser.js'}, function() {
+			chrome.tabs.insertCSS(sender.tab.id, {file: 'dialog.css'}, function () {
+				chrome.pageAction.setIcon({
+					tabId: sender.tab.id,
+					path: {
+						20: 'images/logo-20.png',
+						40: 'images/logo-40.png',
+						60: 'images/logo-60.png',
+						80: 'images/logo-80.png',
+						200: 'images/logo-200.png',
+						668: 'images/logo-668.png',
+					},
+				});
+			});
+		});
+	} else if (msg.text === 'unfocus_video_tab') {
+		chrome.tabs.update(backgroundSenderTabId, { active: true });
+	} else if (msg.text === 'close_video_tab') {
+		kazDebug('Closing tab again');
+		chrome.tabs.remove(sender.tab.id);
+
+		if (episodeList.length > 0) {
 			reportResetStatus();
-			resetEpisodes();
-		} else if (msg.text === 'hide_page_action') {
-			kazDebug('hiding tab with id ' + sender.tab.id);
-			chrome.pageAction.hide(sender.tab.id);
-			chrome.pageAction.setIcon({
-				tabId: sender.tab.id,
-				path: {
-					20: 'images/logo-20-gray.png',
-					40: 'images/logo-40-gray.png',
-					60: 'images/logo-60-gray.png',
-					80: 'images/logo-80-gray.png',
-					200: 'images/logo-200-gray.png',
-					668: 'images/logo-668-gray.png',
-				},
+			setTimeout(function () {
+				resetEpisodes();
+			}, 1000);
+		} else {
+			chrome.tabs.sendMessage(backgroundSenderTabId, {
+				text: 'report_reset_finished',
 			});
-		} else if (msg.text === 'show_page_action') {
-			kazDebug('showing tab with id ' + sender.tab.id);
-			chrome.pageAction.show(sender.tab.id);
-			chrome.tabs.executeScript(sender.tab.id, {file: 'parser.js'}, function() {
-				chrome.tabs.insertCSS(sender.tab.id, {file: 'dialog.css'}, function () {
-					chrome.pageAction.setIcon({
-						tabId: sender.tab.id,
-						path: {
-							20: 'images/logo-20.png',
-							40: 'images/logo-40.png',
-							60: 'images/logo-60.png',
-							80: 'images/logo-80.png',
-							200: 'images/logo-200.png',
-							668: 'images/logo-668.png',
-						},
-					});
-				});
-			});
-		} else if (msg.text === 'unfocus_video_tab') {
-			chrome.tabs.update(backgroundSenderTabId, { active: true });
-		} else if (msg.text === 'close_video_tab') {
-			kazDebug('Closing tab again');
-			chrome.tabs.remove(sender.tab.id);
-
-			if (episodeList.length > 0) {
-				reportResetStatus();
-				setTimeout(function () {
-					resetEpisodes();
-				}, 1000);
-			} else {
-				chrome.tabs.sendMessage(backgroundSenderTabId, {
-					text: 'report_reset_finished',
-				});
-			}
 		}
+	}
 
-	});
 });
 
 
